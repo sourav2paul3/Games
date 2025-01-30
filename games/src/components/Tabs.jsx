62c45c0b74fd834be4d8f723fa56a6e192273874
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-01-30 23:39:31 +0530

    tab select

--- games/src/components/Tabs.jsx
+++ games/src/components/Tabs.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "../Styles/Tabs.module.css";
+import { GamesContext } from "../context/GamesContext";
 
 const Tabs = () => {
+  const { currentTab, setCurrentTab } = useContext(GamesContext);
   return (
     <div className={styles.tabContainer}>
-      <h1 className={styles.tabsHeader}>All Games</h1>
-      <h1 className={styles.tabsHeader}>Favourite Games</h1>
+      <h1
+        className={`${styles.tabsHeader} ${
+          currentTab === "allGames" ? styles.activeTab : ""
+        }`}
+        onClick={() => setCurrentTab("allGames")}
+      >
+        All Games
+      </h1>
+      <h1
+        className={`${styles.tabsHeader} ${
+          currentTab === "favGames" ? styles.activeTab : ""
+        }`}
+        onClick={() => setCurrentTab("favGames")}
+      >
+        Favourite Games
+      </h1>
     </div>
   );
 };
--- games/src/context/GamesContext.jsx
+++ games/src/context/GamesContext.jsx
@@ -4,9 +4,12 @@ export const GamesContext = createContext("");
 
 export const GamesContextProvider = ({ children }) => {
   const [searchedGame, setSearchedGame] = useState("");
+  const [currentTab, setCurrentTab] = useState("allGames");
 
   return (
-    <GamesContext.Provider value={{ searchedGame, setSearchedGame }}>
+    <GamesContext.Provider
+      value={{ searchedGame, setSearchedGame, currentTab, setCurrentTab }}
+    >
       {children}
     </GamesContext.Provider>
   );

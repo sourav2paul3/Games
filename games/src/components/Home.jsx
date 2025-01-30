4a6c7977ba282ea932bf2b41ac60c8f1550fb159
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-01-31 02:27:58 +0530

    Fixes

--- games/src/components/Home.jsx
+++ games/src/components/Home.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar } from "./Navbar";
 import Tabs from "./Tabs";
 import GameCard from "./GameCard";
 import { Gamedata } from "../GameData/GameData.js";
 import styles from "../Styles/Home.module.css";
+import { GamesContext } from "../context/GamesContext.jsx";
 const Home = () => {
+  const { allGames, setAllGames } = useContext(GamesContext);
+  setAllGames(Gamedata);
   return (
     <div>
       <Navbar />
       <Tabs />
       <div className={styles.gameCardContainer}>
-        {Gamedata.map((data, index) => (
+        {allGames.map((data, index) => (
           <GameCard key={index} data={data} />
         ))}
       </div>

12e4115a2449d65bfa65de82bf928b8dd679a907
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-01-31 12:33:52 +0530

    game links

--- games/src/components/Home.jsx
+++ games/src/components/Home.jsx
@@ -6,11 +6,14 @@ import styles from "../Styles/Home.module.css";
 import { GamesContext } from "../context/GamesContext.jsx";
 const Home = () => {
   const { allGames, currentTab } = useContext(GamesContext);
+  const handleCardClick = () => {
+    window.location.href = "https://google.com";
+  };
   return (
     <div>
       <Navbar />
       <Tabs />
-      <div className={styles.gameCardContainer}>
+      <div className={styles.gameCardContainer} onClick={handleCardClick}>
         {currentTab === "allGames"
           ? allGames.map((data, index) => <GameCard key={index} data={data} />)
           : allGames.map((data, index) =>
